2c63da31963da2d72e4b24cd4729ff44e183b883
GrantLudwig <grantludwig97@gmail.com>
2020-01-25 20:18:09 -0800

    bug fixes

--- BitonicPipeline.java
+++ BitonicPipeline.java
@@ -10,6 +10,18 @@ public class BitonicPipeline {
     public static final int TIME_ALLOWED = 10;  // seconds
     //public static final int TIME_ALLOWED = 1;
 
+    Thread  rand1,
+            rand2,
+            rand3,
+            rand4,
+            t1,
+            t2,
+            t3,
+            t4,
+            t5,
+            t6,
+            t7;
+
     // Would like to create an array of SynchronousQueues but cannot find out how
     SynchronousQueue<double[]> inputQueue1 = new SynchronousQueue<double[]>();
     SynchronousQueue<double[]> inputQueue2 = new SynchronousQueue<double[]>();
@@ -28,44 +40,37 @@ public class BitonicPipeline {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
+
+        // Setup all threads
+        // get random numbers
+        rand1 = new Thread(new RandomArrayGenerator(N / 4, inputQueue1));
+        rand1.start();
+        rand2 = new Thread(new RandomArrayGenerator(N / 4, inputQueue2));
+        rand2.start();
+        rand3 = new Thread(new RandomArrayGenerator(N / 4, inputQueue3));
+        rand3.start();
+        rand4 = new Thread(new RandomArrayGenerator(N / 4, inputQueue4));
+        rand4.start();
+
+        t1 = new Thread(new StageOne(inputQueue1, outputQueue1));
+        t1.start();
+        t2 = new Thread(new StageOne(inputQueue2, outputQueue2));
+        t2.start();
+        t3 = new Thread(new StageOne(inputQueue3, outputQueue3));
+        t3.start();
+        t4 = new Thread(new StageOne(inputQueue4, outputQueue4));
+        t4.start();
+
+        t5 = new Thread(new BitonicStage(outputQueue1, outputQueue2, tempOutputQueues1));
+        t5.start();
+        t6 = new Thread(new BitonicStage(outputQueue3, outputQueue4, tempOutputQueues2));
+        t6.start();
+        t7 = new Thread(new BitonicStage(tempOutputQueues1, tempOutputQueues2, finalOutputQueue));
+        t7.start();
+
+        double[] array = new double[N];
         int work = 0;
         while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
-            // get random numbers
-            for (int i = 0; i < 4; i++) {
-                RandomArrayGenerator randNum;
-                if (i == 0)
-                    randNum = new RandomArrayGenerator(N / 4, inputQueue1);
-                else if (i == 1)
-                    randNum = new RandomArrayGenerator(N / 4, inputQueue2);
-                else if (i == 2)
-                    randNum = new RandomArrayGenerator(N / 4, inputQueue3);
-                else
-                    randNum = new RandomArrayGenerator(N / 4, inputQueue4);
-                randNum.start();
-            }
-            for (int i = 0; i < 4; i++) {
-                StageOne stage;
-                if (i == 0)
-                    stage = new StageOne(inputQueue1, outputQueue1);
-                else if (i == 1)
-                    stage = new StageOne(inputQueue2, outputQueue2);
-                else if (i == 2)
-                    stage = new StageOne(inputQueue3, outputQueue3);
-                else
-                    stage = new StageOne(inputQueue4, outputQueue4);
-                stage.start();
-            }
-
-            BitonicStage bitonic1 = new BitonicStage(outputQueue1, outputQueue2, tempOutputQueues1);
-            bitonic1.start();
-
-            BitonicStage bitonic2 = new BitonicStage(outputQueue3, outputQueue4, tempOutputQueues2);
-            bitonic2.start();
-
-            BitonicStage bitonicFinal = new BitonicStage(tempOutputQueues1, tempOutputQueues2, finalOutputQueue);
-            bitonicFinal.start();
-
-            double[] array = new double[N];
             finalOutputQueue.offer(array, timeout * 1000, TimeUnit.MILLISECONDS);
 
             if (!RandomArrayGenerator.isSorted(array) || N != array.length)
