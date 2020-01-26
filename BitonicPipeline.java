e149e53b0fd8a0fea836f99ee600b7cc902fd078
GrantLudwig <grantludwig97@gmail.com>
2020-01-25 20:04:03 -0800

    bug fixes

--- BitonicPipeline.java
+++ BitonicPipeline.java
@@ -9,19 +9,21 @@ public class BitonicPipeline {
     //public static final int N = 16;
     public static final int TIME_ALLOWED = 10;  // seconds
     //public static final int TIME_ALLOWED = 1;
-    SynchronousQueue<double[]> inputQueue[];
-    inputQueue[0] = new SynchronousQueue<double[]>();
-    inputQueue[1] = new SynchronousQueue<double[]>();
-    inputQueue[2] = new SynchronousQueue<double[]>();
-    inputQueue[3] = new SynchronousQueue<double[]>();
-    SynchronousQueue<double[]>[] outputQueue;
-    outputQueue[0] = new SynchronousQueue<double[]>();
-    outputQueue[1] = new SynchronousQueue<double[]>();
-    outputQueue[2] = new SynchronousQueue<double[]>();
-    outputQueue[3] = new SynchronousQueue<double[]>();
-    SynchronousQueue<double[]>[] tempOutputQueues;
-    tempOutputQueues[0] = new SynchronousQueue<double[]>();
-    tempOutputQueues[1] = new SynchronousQueue<double[]>();
+
+    // Would like to create an array of SynchronousQueues but cannot find out how
+    SynchronousQueue<double[]> inputQueue1 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> inputQueue2 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> inputQueue3 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> inputQueue4 = new SynchronousQueue<double[]>();
+
+    SynchronousQueue<double[]> outputQueue1 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> outputQueue2 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> outputQueue3 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> outputQueue4 = new SynchronousQueue<double[]>();
+
+    SynchronousQueue<double[]> tempOutputQueue1 = new SynchronousQueue<double[]>();
+    SynchronousQueue<double[]> tempOutputQueue2 = new SynchronousQueue<double[]>();
+
     SynchronousQueue<double[]> finalOutputQueue = new SynchronousQueue<double[]>();
 
     public static void main(String[] args) {
@@ -29,22 +31,38 @@ public class BitonicPipeline {
         int work = 0;
         while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
             // get random numbers
-            for (int i = 0; i < inputQueue.length; i++) {
-                RandomArrayGenerator randNum = new RandomArrayGenerator(N / 4, inputQueue[i]);
+            for (int i = 0; i < 4; i++) {
+                RandomArrayGenerator randNum;
+                if (i == 0)
+                    randNum = new RandomArrayGenerator(N / 4, inputQueue1);
+                else if (i == 1)
+                    randNum = new RandomArrayGenerator(N / 4, inputQueue2);
+                else if (i == 2)
+                    randNum = new RandomArrayGenerator(N / 4, inputQueue3);
+                else
+                    randNum = new RandomArrayGenerator(N / 4, inputQueue4);
                 randNum.start();
             }
-            for (int i = 0; i < inputQueue.length; i++) {
-                StageOne stage = new StageOne(inputQueue[i], outputQueue[i]);
+            for (int i = 0; i < 4; i++) {
+                StageOne stage;
+                if (i == 0)
+                    stage = new StageOne(inputQueue1, outputQueue1);
+                else if (i == 1)
+                    stage = new StageOne(inputQueue2, outputQueue2);
+                else if (i == 2)
+                    stage = new StageOne(inputQueue3, outputQueue3);
+                else
+                    stage = new StageOne(inputQueue4, outputQueue4);
                 stage.start();
             }
 
-            BitonicStage bitonic1 = new BitonicStage(outputQueue[0], outputQueue[1], tempOutputQueues[0]);
+            BitonicStage bitonic1 = new BitonicStage(outputQueue1, outputQueue2, tempOutputQueues1);
             bitonic1.start();
 
-            BitonicStage bitonic2 = new BitonicStage(outputQueue[2], outputQueue[3], tempOutputQueues[1]);
+            BitonicStage bitonic2 = new BitonicStage(outputQueue3, outputQueue4, tempOutputQueues2);
             bitonic2.start();
 
-            BitonicStage bitonicFinal = new BitonicStage(tempOutputQueues[0], tempOutputQueues[1], finalOutputQueue);
+            BitonicStage bitonicFinal = new BitonicStage(tempOutputQueues1, tempOutputQueues2, finalOutputQueue);
             bitonicFinal.start();
 
             double[] array = new double[N];
