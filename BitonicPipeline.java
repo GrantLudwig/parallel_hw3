80fd72c4b7f9cd5ec2e570ea892b7d1a920b6145
GrantLudwig <grantludwig97@gmail.com>
2020-01-25 21:31:37 -0800

    bug fixes

--- BitonicPipeline.java
+++ BitonicPipeline.java
@@ -81,7 +81,7 @@ public class BitonicPipeline {
 
         while (System.currentTimeMillis() < start + TIME_ALLOWED * 1000) {
             try {
-                finalOutputQueue.poll(array, timeout * 1000, TimeUnit.MILLISECONDS);
+                array = finalOutputQueue.poll(timeout * 1000, TimeUnit.MILLISECONDS);
 
                 if (!RandomArrayGenerator.isSorted(array) || N != array.length)
                     System.out.println("failed");
